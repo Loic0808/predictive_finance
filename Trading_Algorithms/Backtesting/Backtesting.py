5284bc0287bf3b9c3e7be6712b1d993391bf7774
Loic Dobler <loudopi@gmail.com>
2024-12-23 15:28:47 +0100

    test backtesting

--- Trading_Algorithms/Backtesting/Backtesting.py
+++ Trading_Algorithms/Backtesting/Backtesting.py
@@ -2,3 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("Trading_Algorithms/Backtesting/Strat_dataframes/ppo_MSFT_15m_v1_run1.csv")  
 
+# 4 cases: 
+# If multiple sell and then buy: if buy > sell -> profit
+#                                else -> loss
+# If multiple buy and then sell: if sell > buy -> profit
+#                                else -> loss
